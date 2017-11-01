7349f287bc6af5d14eeace80415df8863423d537
rodolfoo30 <rodolfoalmeida.tecinfo@gmail.com>
2017-11-01 08:40:24 -0300

    Estágio 2

---
+++ Estágio 2/Exercício 10 - Chain of Responsibility/FilterOp.java
@@ -0,0 +1,15 @@
+public abstract class FilterOp {
+	private FilterOp nextOp;
+
+	public FilterOp nextOp(FilterOp o) {
+		nextOp = o;
+		return o;
+	}
+
+	public void apply(Imagem img) throws FilterError {
+		op(img);
+		nextOp.apply(img);
+	}
+
+	public abstract void op(Imagem i) throws FilterError;
+}
---
+++ Estágio 2/Exercício 10 - Chain of Responsibility/FilterSky.java
@@ -0,0 +1,15 @@
+
+public class FilterSky extends FilterOp {
+
+	private String color;
+
+	public FilterSky(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public void op(Imagem i) throws FilterError {
+
+	}
+
+}
